5314e44eccd46184e401a41362f02727d5cf1320
Ihor Yakovenko <igorakovenko982@gmail.com>
2024-12-19 15:29:37 +0100

    Fixed CompileShader.bat

--- BMEngine/Source/BMR/Shaders/CompileShaders.bat
+++ BMEngine/Source/BMR/Shaders/CompileShaders.bat
@@ -3,6 +3,7 @@
 
 if "%VULKAN_SDK%" == "" (
     echo VULKAN_SDK environment variable is not set.
+    pause
     exit /b 1
 )
 
@@ -10,11 +11,21 @@ set PROJECT_DIR=%1
 
 if "%PROJECT_DIR%" == "" (
     echo PROJECT_DIR environment variable is not set.
+    pause
     exit /b 2
 )
 
 set SHADER_PATH=%PROJECT_DIR%Source/BMR/Shaders/
 set SHADER_OUTPUT=%PROJECT_DIR%/Resources/Shaders/
+
+if not exist "%SHADER_OUTPUT%" (
+    mkdir "%SHADER_OUTPUT%"
+    if errorlevel 1 (
+        echo Failed to create directory "%SHADER_OUTPUT%".
+        pause
+        exit /b 3
+    )
+)
 
 %VULKAN_SDK%/Bin/glslangValidator.exe -o %SHADER_OUTPUT%vert.spv -V %SHADER_PATH%Entity.vert.glsl
 %VULKAN_SDK%/Bin/glslangValidator.exe -o %SHADER_OUTPUT%frag.spv -V %SHADER_PATH%Entity.frag.glsl
